2da43b292fc659ffaefe40c794118fa036f87ef9
gbourdon <34389449+JamesRevere@users.noreply.github.com>
2017-12-09 16:23:59 -0700

    Clearing out the rubbish
    General updating of the bot for non-exclusive use

--- code/bot.rb
+++ code/bot.rb
@@ -21,7 +21,7 @@ required_permissions: [:ban_members]
 
 bot.command(:about, description: "Tells a bit about the bot") do #Tells a bit about the bot.
     "Hello.
-I am **Cringe Kid**, a testing bot in discordrb created by the one and only gustav341. 
+I am **GreenThumb**, a bot in discordrb created by the one and only gustav341. 
 Why? For world domination, of course!
 (Actually, it's to do some useful stuff)
 If you want to use me, do .help" #Tells you about the bot
@@ -34,13 +34,13 @@ bot.command(:ping, description: "Sends a pong if the bot is responding.", permis
     
 end
 
-bot.command(:clear, description: "Syntax: .clear n\nClears out n messages. Will require something special in the future.", permission_level: 1, required_permissions: [:manage_messages]) do |event, num| #Clears n messages, including itself
+bot.command(:clear, description: "Syntax: .clear n\nClears out n messages.", permission_level: 0, required_permissions: [:manage_messages]) do |event, num| #Clears n messages, including itself
     num = num.to_i #Turns the string into something you can work with
     num += 1 #Includes the orgin message
     event.channel.prune(num) #Prunes (clears) num messages.
 end
 
-bot.command(:ban, description: "Syntax: .ban <user> <reason>\nBans <user> for <reason>", permission_level: 1, required_permissions: [:ban_members]) do |event, mention, *reason| #Bans a mentioned member
+bot.command(:ban, description: "Syntax: .ban <user> <reason>\nBans <user> for <reason>", permission_level: 0, required_permissions: [:ban_members]) do |event, mention, *reason| #Bans a mentioned member
     member = event.bot.parse_mention(mention) #Converts the mention into a USER object
     if event.user == member #Stops the user from inadvertently banning themselves
       event.respond("Stop banning yourself, #{mention}")
@@ -52,7 +52,7 @@ bot.command(:ban, description: "Syntax: .ban <user> <reason>\nBans <user> for <r
     #TODO: Rename the member variables to user in Ban and Kick to avoid logic errors.
 end
 
-bot.command(:kick, description: "Syntax: .kick <user> <reason>\nBans <user> for <reason>", permission_level: 1, required_permissions: [:kick_members]) do |event, mention, *reason| #Kicks a mentioned member
+bot.command(:kick, description: "Syntax: .kick <user> <reason>\nBans <user> for <reason>", permission_level: 0, required_permissions: [:kick_members]) do |event, mention, *reason| #Kicks a mentioned member
     member = event.bot.parse_mention(mention) #Converts the mention into a USER object
     if event.user == member #Stops the user from inadvertently kicking themselves
         event.respond("Stop kicking yourself, #{mention}")
@@ -60,17 +60,15 @@ bot.command(:kick, description: "Syntax: .kick <user> <reason>\nBans <user> for 
     end
     #event.server.kick(member, reason.join(" "))
     event.server.kick(member) #Kickes the user
-    event.respond reason == [] ? "Kicked #{mention}" : "Kicked #{mention} for #{reason.join(" ")}" #Informs the person that the use has been kicked
+    event.respond reason == [] ? "Kicked #{mention}" : "Kicked #{mention} for #{reason.join(" ")}" #Informs the person that the user has been kicked
 end
 
 
 bot.ready do #When the bot is initalised
     bot.game = (".help") #Sets the "Playing" screen to '.help'
-    bot.set_user_permission(277844547604840460, 10) #Gives my alt special powers
-    bot.set_user_permission(181225342269259776, 10) #Gives yours truly god
 end
 
-bot.command(:roleid, permission_level: 1, description: "Syntax: .roleid Rolename\nReturns the role id of Rolename.") do |event, *id| #Returns the Role's id
+bot.command(:roleid, permission_level: 0, description: "Syntax: .roleid Rolename\nReturns the role id of Rolename.") do |event, *id| #Returns the Role's id
     id = id.join(" ")
     roles = event.server.roles
     roles = roles.select{ |role| role.name == id }
